711b1a072993e060f85db824c9d40e7f9f134ca6
N-SPC700 <marioworldsmwc@gmail.com>
2020-04-05 22:47:43 -0400

    Add files via upload
    Initial Beta

---
+++ oscview.bat
@@ -0,0 +1,24 @@
+<# : chooser.bat
+@echo off
+setlocal
+
+for /f "delims=" %%I in ('powershell -noprofile "iex (${%~f0} | out-string)"') do (
+    	rem >replace the time with the song's length
+	rem >and replace the .sid name with the name of the song.
+	start /b sidplayfp -t4:30 -f96000 -wmaster %%~I
+	start /b sidplayfp -t4:30 -f96000 -wch1 -u2 -u3 %%~I
+	start /b sidplayfp -t4:30 -f96000 -wch2 -u1 -u3 %%~I
+	start /b sidplayfp -t4:30 -f96000 -wch3 -u2 -u1 %%~I
+)
+goto :EOF
+
+: end Batch portion / begin PowerShell hybrid chimera #>
+
+Add-Type -AssemblyName System.Windows.Forms
+$f = new-object Windows.Forms.OpenFileDialog
+$f.InitialDirectory = pwd
+$f.Filter = "Commodore 64 SID Files (*.sid)|*.sid|All Files (*.*)|*.*"
+$f.ShowHelp = $true
+$f.Multiselect = $true
+[void]$f.ShowDialog()
+if ($false) { $f.FileNames } else { $f.FileName }
