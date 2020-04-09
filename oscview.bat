40d25816b9bd2da208d9f1ecae2d2f6362265f37
N-SPC700 <marioworldsmwc@gmail.com>
2020-04-09 14:11:17 -0400

    add track selection
    add track selection and fix export garbage and input breaking bugs

--- oscview.bat
+++ oscview.bat
@@ -1,15 +1,14 @@
 <# : chooser.bat
 @echo off 
 set /p _songlength= Enter song length (Syntax: mm:ss):
+set /p _tracknum= Enter track number:
 setlocal
 
 for /f "delims=" %%I in ('powershell -noprofile "iex (${%~f0} | out-string)"') do (
-    	rem >replace the time with the song's length
-	rem >and replace the .sid name with the name of the song.
-	start /b sidplayfp -t"%_songlength%" -f96000 -wmaster %%~I
-	start /b sidplayfp -t"%_songlength%" -f96000 -wch1 -u2 -u3 %%~I
-	start /b sidplayfp -t"%_songlength%" -f96000 -wch2 -u1 -u3 %%~I
-	start /b sidplayfp -t"%_songlength%" -f96000 -wch3 -u2 -u1 %%~I
+	start "" cmd /b /c "echo. | sidplayfp -o"%_tracknum%" -t"%_songlength%" -f96000 -wmaster %%~I 2>NUL"
+	start "" cmd /b /c "echo. | sidplayfp -o"%_tracknum%" -t"%_songlength%" -f96000 -wch1 -u2 -u3 %%~I 2>NUL"
+	start "" cmd /b /c "echo. | sidplayfp -o"%_tracknum%" -t"%_songlength%" -f96000 -wch2 -u1 -u3 %%~I 2>NUL"
+	start "" cmd /b /c "echo. | sidplayfp -o"%_tracknum%" -t"%_songlength%" -f96000 -wch3 -u2 -u1 %%~I 2>NUL"
 )
 goto :EOF
 
